1c161a9da95f4b4818a6a74491fcd5e113a79f05
BaoTranVQ <tranvanquocbao333@gmail.com>
2023-07-26 22:15:19 +0700

    video5

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+const port = 3000
 
-const sever = http.createServer((req,res) =>{
-    console.log('run request ...');
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello World</h3>');
-    res.write('<h2> from Eric & Quoc Bao</h2>');
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
-Server.listen(3000,'localhost',()=>{
-    console.log('Node.JS server is running on prot: 3000');
+app.get('/about', (req, res) => {
+    res.send('Iam Bao')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
